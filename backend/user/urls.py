24135725208019d0730707fbfe767d3ed7ae35cb
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-12-15 17:08:39 +0600

    feat: user account activation

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import (
     UserRegistrationView,
+    UserActivationView,
 )
 
 
@@ -8,6 +9,10 @@ urlpatterns = [
     # POST: 127.0.0.1:8000/user/register/
     path(
         route="register/", view=UserRegistrationView.as_view(), name="user_registration"
+    ),
+    # POST: 127.0.0.1:8000/user/activate/
+    path(
+        route="activate/", view=UserActivationView.as_view(), name="user_activation"
     ),
 
 ]
--- backend/user/views.py
+++ backend/user/views.py
@@ -2,8 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.exceptions import ValidationError
+from django.shortcuts import get_object_or_404
+from datetime import datetime, timedelta
+from django.utils import timezone
 from .serializers import UserRegistrationSerializer
 from otp.otp_send import otp_send
+from otp.models import OTPModel
+from user.models import UserAccount
 
 
 
@@ -40,3 +45,44 @@ class UserRegistrationView(APIView):
             otp_send(email)
             
             return Response("succesfull! user is created")
+
+
+class UserActivationView(APIView):
+    """User can activate account by OTP received through email."""
+
+    permission_classes = [AllowAny]
+
+    def patch(self, request, *args, **kwargs):
+        """Used to update user activate status by otp confirm"""
+
+        email = request.data.get("email")
+        otp = request.data.get("otp")
+
+        if not email or not otp:
+            raise ValidationError("Please provide both email and OTP.")
+
+        otp_obj = get_object_or_404(OTPModel, email=email, otp=otp)
+
+        now_date = datetime.now().date()
+        now_time = datetime.now().time().strftime("%H:%M:%S")
+
+        otp_obj_date = otp_obj.created_at.date()
+        validation_time = timezone.localtime(otp_obj.created_at) + timedelta(minutes=40)
+        validation_time = validation_time.time().strftime("%H:%M:%S")
+
+        if now_date != otp_obj_date:
+            otp_obj.delete()
+
+            return Response("Timeout!, OTP has expired.")
+
+        if now_time <= validation_time:
+            otp_obj.delete()
+
+            return Response("Timeout!, OTP has expired.")
+
+        user = UserAccount.objects.get(email=email)
+        user.is_active = True
+        user.save()
+        otp_obj.delete()
+
+        return Response("Your account has been activated!")
