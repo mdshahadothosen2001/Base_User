ac4c89c866e245a4b9b3262575e6b9fa72fc1038
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-02-26 10:32:51 +0600

    chore: password api modified

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -18,11 +18,11 @@ urlpatterns = [
     ),
     # POST: localhost:8000/user/activate/
     path(route="activate/", view=UserActivationView.as_view(), name="user_activation"),
-    # POST: localhost:8000/user/reset/
-    path(route="reset/", view=UserPasswordResetView.as_view(), name="password_reset"),
-    # POST: localhost:8000/user/forgotten/
+    # POST: localhost:8000/user/password/reset/
+    path(route="password/reset/", view=UserPasswordResetView.as_view(), name="password_reset"),
+    # POST: localhost:8000/user/password/forgotten/
     path(
-        route="forgotten/",
+        route="password/forgotten/",
         view=ForgottenPasswordResetView.as_view(),
         name="password_forgotten",
     ),
@@ -32,6 +32,6 @@ urlpatterns = [
         view=UpdateProfileView.as_view(),
         name="update_profile",
     ),
-    # localhost:8000/user/
+    # localhost:8000/user/token/
     path("token/", include("user.token_api.urls")),
 ]
