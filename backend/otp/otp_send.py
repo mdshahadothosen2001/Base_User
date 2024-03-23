9b100ce1f5d56fabb0685cd1febef435deddaa58
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-03-23 12:40:44 +0600

    chore: otp generate and sending method modified

--- backend/otp/otp_send.py
+++ backend/otp/otp_send.py
@@ -10,42 +10,18 @@ def send_otp_to_email(email):
     """Used to generate otp and send through email"""
 
     otp = random.randint(1000, 9999)
-
-    try:
-        subject = "Your OTP for registration"
-        message = f"Your OTP is: {otp}"
-        from_email = settings.EMAIL_HOST
-        if EMAIL_HOST_USER and EMAIL_HOST_PASSWORD:
-            send_mail(subject, message, from_email, [email])
-        print(
-            "\n\n\n............................................................................"
-        )
-        print(".")
-        print(".")
-        print(".")
-        print(f"Here your {otp} OTP successfully sent to {email}")
-        print(".")
-        print(".")
-        print(".")
-        print(
-            "............................................................................\n\n\n"
-        )
-
-        return otp
-
-    except Exception as e:
-        print("Error sending OTP :", e)
+    subject = "Your OTP for account activation"
+    message = f"Your OTP is: {otp}"
+    from_email = settings.EMAIL_HOST
+    if EMAIL_HOST_USER and EMAIL_HOST_PASSWORD:
+        send_mail(subject, message, from_email, [email])
+    return otp
 
 
 def otp_send(email):
     """Used to call send otp function and save otp to the otp model"""
 
     otp = send_otp_to_email(email)
-
-    try:
-        otp_instance = OTPModel.objects.get(email=email)
-        otp_instance.otp = otp
-        otp_instance.save()
-
-    except OTPModel.DoesNotExist:
-        otp_instance = OTPModel.objects.create(email=email, otp=otp)
+    otp_instance, created = OTPModel.objects.get_or_create(email=email)
+    otp_instance.otp = otp
+    otp_instance.save()
