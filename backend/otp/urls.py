7debfa60c4b633ce52321dea8a8c5bea5628ad33
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-03-24 09:17:30 +0600

    chore: otp app simplified

--- backend/otp/urls.py
+++ backend/otp/urls.py
@@ -4,6 +4,6 @@ from .views import ResentOTPView
 
 
 urlpatterns = [
-    # POST: 127.0.0.1:8000/otp/resend/
-    path(route="resend/", view=ResentOTPView.as_view(), name="otp-resend"),
+    # POST: 127.0.0.1:8091/otp/resend/
+    path(route="resend/", view=ResentOTPView.as_view(), name="otp_resend"),
 ]
