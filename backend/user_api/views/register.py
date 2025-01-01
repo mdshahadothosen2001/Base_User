ff653b547f255b0ed2683d4ac6034c17cc6fd6b6
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2025-01-01 20:06:17 +0600

    chore: make simplier code

--- backend/user_api/views/register.py
+++ backend/user_api/views/register.py
@@ -10,8 +10,6 @@ from utils.utils import generate_otp_and_otp_send_to_email
 
 
 class UserRegistrationView(APIView):
-    """Users can register their account by email, frist_name, last_name and password."""
-
     permission_classes = [AllowAny]
 
     def validate_parameter(self, phone_number, email, password):
@@ -34,7 +32,12 @@ class UserRegistrationView(APIView):
 
         if self.validate_parameter(phone_number, email, password) is True:
             if self.have_account(phone_number, email) is True:
-                return Response("You have already account at SeeHouse")
+                return Response(
+                    {
+                        "success": False,
+                        "message": "Incompleted process! You have already account at SeeHouse",
+                    }
+                )
             user_data = {
                 "phone_number": phone_number,
                 "email": email,
@@ -45,7 +48,16 @@ class UserRegistrationView(APIView):
             if serializer.is_valid():
                 serializer.save()
                 generate_otp_and_otp_send_to_email(email)
+                return Response(
+                    {
+                        "success": True,
+                        "message": "Completed process! your account has created",
+                    }
+                )
 
-                return Response("Completed your registration process!")
-
-        return Response("Incompleted registration! Please provide valid data")
+        return Response(
+            {
+                "success": False,
+                "message": "Incompleted process! Please provide valid data",
+            }
+        )
