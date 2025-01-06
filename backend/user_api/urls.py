2ca4ceb80eb0d9673af359edea773b3b2865f3d4
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2025-01-06 08:22:44 +0600

    chore: remove comments from urls

--- backend/user_api/urls.py
+++ backend/user_api/urls.py
@@ -12,34 +12,26 @@ from user_api.views.otp import ResendOTPView
 
 
 urlpatterns = [
-    # POST: localhost:8091/user/register/
     path(
         route="register/", view=UserRegistrationView.as_view(), name="user_registration"
     ),
-    # POST: localhost:8091/user/account/otp/resend/
     path(route="account/otp/resend/", view=ResendOTPView.as_view(), name="otp_resend"),
-    # POST: localhost:8091/user/activate/
     path(route="activate/", view=UserActivationView.as_view(), name="user_activation"),
-    # POST: localhost:8091/user/password/change/
     path(
         route="password/change/",
         view=UserPasswordChangeView.as_view(),
         name="password_change",
     ),
-    # POST: localhost:8091/user/password/recover/
     path(
         route="password/recover/",
         view=UserPasswordRecoverView.as_view(),
         name="password_recover",
     ),
-    # PATCH: localhost:8091/user/profile/update/
     path(
         route="profile/update/",
         view=UserProfileUpdateView.as_view(),
         name="update_profile",
     ),
-    # POST: localhost:8091/user/token/
     path(route="token/", view=CustomTokenObtainPairView.as_view(), name="token"),
-    # POST: localhost:8091/user/token/refresh/
     path(route="token/refresh/", view=TokenRefreshView.as_view(), name="token_refresh"),
 ]
